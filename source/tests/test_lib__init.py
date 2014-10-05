8aac8a88d89827f9086c3c2ed454f7a7c70ec38f
Janyell <e.sycheva_nn@mail.ru>
2014-10-05 13:10:28 +0400

    lib: 98% 100% 93%

--- source/tests/test_lib__init.py
+++ source/tests/test_lib__init.py
@@ -170,13 +170,10 @@ class LibInitTestCase(unittest.TestCase):
         curl.setopt = mock.Mock()
         curl.perform = mock.Mock()
         curl.getinfo = mock.Mock(return_value=redirect_url)
-
-        with mock.patch('source.lib.to_str', mock.Mock(return_value=url)):
-            with mock.patch('source.lib.StringIO', mock.Mock(return_value=buff)):
-                with mock.patch('pycurl.Curl', mock.Mock(return_value=curl)):
-                    with mock.patch('source.lib.to_unicode', mock.Mock(return_value=redirect_url)):
-                        self.assertEquals((content, redirect_url), lib.make_pycurl_request(url, timeout))
-                        self.assertEquals((content, redirect_url), lib.make_pycurl_request(url, timeout, useragent))
+        with mock.patch('source.lib.StringIO', mock.Mock(return_value=buff)):
+            with mock.patch('pycurl.Curl', mock.Mock(return_value=curl)):
+                self.assertEquals((content, redirect_url), lib.make_pycurl_request(url, timeout))
+                self.assertEquals((content, redirect_url), lib.make_pycurl_request(url, timeout, useragent))
 
     def test_make_pycurl_request_none(self):
         url = 'http://url.ru'
@@ -189,22 +186,205 @@ class LibInitTestCase(unittest.TestCase):
         curl.setopt = mock.Mock()
         curl.perform = mock.Mock()
         curl.getinfo = mock.Mock(return_value=None)
-
-        with mock.patch('source.lib.to_str', mock.Mock(return_value=url)):
-            with mock.patch('source.lib.StringIO', mock.Mock(return_value=buff)):
-                with mock.patch('pycurl.Curl', mock.Mock(return_value=curl)):
-                    self.assertEquals((content, None), lib.make_pycurl_request(url, timeout))
-                    self.assertEquals((content, None), lib.make_pycurl_request(url, timeout, useragent))
+        with mock.patch('source.lib.StringIO', mock.Mock(return_value=buff)):
+            with mock.patch('pycurl.Curl', mock.Mock(return_value=curl)):
+                self.assertEquals((content, None), lib.make_pycurl_request(url, timeout))
+                self.assertEquals((content, None), lib.make_pycurl_request(url, timeout, useragent))
 
     #get_url(url, timeout, user_agent=None)
         #positive_tests
-    def test_get_url(self):
-        url = "http://url.ru"
+    def test_get_url_not_redirect(self):
+        url = 'http://url.ru'
         timeout = 30
-        pass
+        content = 'content'
+        new_redirect_url = 'http://odnoklassniki.ru/redirect-url/st.redirect'
+        with mock.patch('source.lib.make_pycurl_request', mock.Mock(return_value=[content, new_redirect_url])):
+            redirect_url, redirect_type, return_content = lib.get_url(url, timeout)
+
+        self.assertEquals(None, redirect_url)
+        self.assertEquals(None, redirect_type)
+        self.assertEquals(content, return_content)
+
+    def test_get_url_redirect_http(self):
+        from source.lib import REDIRECT_HTTP
+        url = 'http://url.ru'
+        timeout = 30
+        content = 'content'
+        new_redirect_url = 'http://redirect-url.ru'
+        with mock.patch('source.lib.make_pycurl_request', mock.Mock(return_value=[content, new_redirect_url])):
+            redirect_url, redirect_type, return_content = lib.get_url(url, timeout)
+
+        self.assertEquals(new_redirect_url, redirect_url)
+        self.assertEquals(REDIRECT_HTTP, redirect_type)
+        self.assertEquals(content, return_content)
+
+    def test_get_url_redirect_http_market(self):
+        from source.lib import REDIRECT_HTTP
+        url = 'http://url.ru'
+        timeout = 30
+        content = 'content'
+        new_redirect_url = 'market://redirect-url.ru'
+        new_redirect_url_without_market = 'http://play.google.com/store/apps/redirect-url.ru'
+        with mock.patch('source.lib.make_pycurl_request', mock.Mock(return_value=[content, new_redirect_url])):
+            redirect_url, redirect_type, return_content = lib.get_url(url, timeout)
+
+        self.assertEquals(new_redirect_url_without_market, redirect_url)
+        self.assertEquals(REDIRECT_HTTP, redirect_type)
+        self.assertEquals(content, return_content)
+
+    def test_get_url_redirect_meta_none(self):
+        url = 'http://url.ru'
+        timeout = 30
+        content = 'content'
+        with mock.patch('source.lib.make_pycurl_request', mock.Mock(return_value=[content, None])):
+            with mock.patch('source.lib.check_for_meta', mock.Mock(return_value=None)):
+                redirect_url, redirect_type, return_content = lib.get_url(url, timeout)
+
+        self.assertEquals(None, redirect_url)
+        self.assertEquals(None, redirect_type)
+        self.assertEquals(content, return_content)
+
+    def test_get_url_redirect_meta(self):
+        from source.lib import REDIRECT_META
+        url = 'http://url.ru'
+        timeout = 30
+        content = 'content'
+        new_redirect_url = 'http://redirect-url.ru'
+        with mock.patch('source.lib.make_pycurl_request', mock.Mock(return_value=[content, None])):
+            with mock.patch('source.lib.check_for_meta', mock.Mock(return_value=new_redirect_url)):
+                redirect_url, redirect_type, return_content = lib.get_url(url, timeout)
+
+        self.assertEquals(new_redirect_url, redirect_url)
+        self.assertEquals(REDIRECT_META, redirect_type)
+        self.assertEquals(content, return_content)
+
+    def test_get_url_redirect_meta_market(self):
+        from source.lib import REDIRECT_META
+        url = 'http://url.ru'
+        timeout = 30
+        content = 'content'
+        new_redirect_url = 'market://redirect-url.ru'
+        new_redirect_url_without_market = 'http://play.google.com/store/apps/redirect-url.ru'
+        with mock.patch('source.lib.make_pycurl_request', mock.Mock(return_value=[content, None])):
+            with mock.patch('source.lib.check_for_meta', mock.Mock(return_value=new_redirect_url)):
+                redirect_url, redirect_type, return_content = lib.get_url(url, timeout)
+
+        self.assertEquals(new_redirect_url_without_market, redirect_url)
+        self.assertEquals(REDIRECT_META, redirect_type)
+        self.assertEquals(content, return_content)
+
+        #negative_tests
+    def test_get_url_value_error(self):
+        url = 'http://url.ru'
+        timeout = 30
+        with mock.patch('source.lib.make_pycurl_request', mock.Mock(side_effect=ValueError)):
+            redirect_url, redirect_type, content = lib.get_url(url, timeout)
+
+        self.assertEquals(url, redirect_url)
+        self.assertEquals('ERROR', redirect_type)
+        self.assertEquals(None, content)
 
     #get_redirect_history(url, timeout, max_redirects=30, user_agent=None)
         #positive_tests
+    def test_get_redirect_history_ok_url(self):
+        url = 'https://www.odnoklassniki.ru/'
+        timeout = 30
+        with mock.patch('source.lib.prepare_url', mock.Mock(return_value=url)):
+            history_types, history_urls, counters = lib.get_redirect_history(url, timeout)
+
+        self.assertEquals([], history_types)
+        self.assertEquals([url], history_urls)
+        self.assertEquals([], counters)
+
+    def test_get_redirect_history_mm_url(self):
+        url = 'https://my.mail.ru/apps/'
+        timeout = 30
+        with mock.patch('source.lib.prepare_url', mock.Mock(return_value=url)):
+            history_types, history_urls, counters = lib.get_redirect_history(url, timeout)
+
+        self.assertEquals([], history_types)
+        self.assertEquals([url], history_urls)
+        self.assertEquals([], counters)
+
+    def test_get_redirect_history_not_redirect_url(self):
+        url = 'http://url.ru'
+        timeout = 30
+        with mock.patch('source.lib.prepare_url', mock.Mock(return_value=url)):
+            with mock.patch('source.lib.get_url', mock.Mock(return_value=[None, None, None])):
+                history_types, history_urls, counters = lib.get_redirect_history(url, timeout)
+
+        self.assertEquals([], history_types)
+        self.assertEquals([url], history_urls)
+        self.assertEquals([], counters)
+
+    def test_get_redirect_history_not_redirect_url_content(self):
+        url = 'http://url.ru'
+        timeout = 30
+        counters = 'counters'
+        with mock.patch('source.lib.prepare_url', mock.Mock(return_value=url)):
+            with mock.patch('source.lib.get_url', mock.Mock(return_value=[None, None, 'content'])):
+                with mock.patch('source.lib.get_counters', mock.Mock(return_value=counters)):
+                    history_types, history_urls, return_counters = lib.get_redirect_history(url, timeout)
+
+        self.assertEquals([], history_types)
+        self.assertEquals([url], history_urls)
+        self.assertEquals(counters, return_counters)
+
+    def test_get_redirect_history_redirect_type_error(self):
+        url = 'http://url.ru'
+        timeout = 30
+        redirect_url = 'http://redirect-url.ru'
+        redirect_type_error = 'ERROR'
+        with mock.patch('source.lib.prepare_url', mock.Mock(return_value=url)):
+            with mock.patch('source.lib.get_url', mock.Mock(return_value=[redirect_url, redirect_type_error, None])):
+                history_types, history_urls, counters = lib.get_redirect_history(url, timeout)
+
+        self.assertEquals([redirect_type_error], history_types)
+        self.assertEquals([url, redirect_url], history_urls)
+        self.assertEquals([], counters)
+
+    def test_get_redirect_history_redirect_type_error_content(self):
+        url = 'http://url.ru'
+        timeout = 30
+        redirect_url = 'http://redirect-url.ru'
+        redirect_type_error = 'ERROR'
+        counters = 'counters'
+        with mock.patch('source.lib.prepare_url', mock.Mock(return_value=url)):
+            with mock.patch('source.lib.get_url', mock.Mock(return_value=[redirect_url, redirect_type_error, 'content'])):
+                with mock.patch('source.lib.get_counters', mock.Mock(return_value=counters)):
+                    history_types, history_urls, return_counters = lib.get_redirect_history(url, timeout)
+
+        self.assertEquals([redirect_type_error], history_types)
+        self.assertEquals([url, redirect_url], history_urls)
+        self.assertEquals(counters, return_counters)
+
+    def test_get_redirect_history_redirect_repeat(self):
+        url = 'http://url.ru'
+        timeout = 30
+        redirect_url = 'http://redirect-url.ru'
+        redirect_type = 'redirect_type'
+        with mock.patch('source.lib.prepare_url', mock.Mock(return_value=url)):
+            with mock.patch('source.lib.get_url', mock.Mock(return_value=[redirect_url, redirect_type, None])):
+                    history_types, history_urls, return_counters = lib.get_redirect_history(url, timeout)
+
+        self.assertEquals([redirect_type, redirect_type], history_types)
+        self.assertEquals([url, redirect_url, redirect_url], history_urls)
+        self.assertEquals([], return_counters)
+
+    def test_get_redirect_history_redirect_repeat_content(self):
+        url = 'http://url.ru'
+        timeout = 30
+        redirect_url = 'http://redirect-url.ru'
+        redirect_type = 'redirect_type'
+        counters = 'counters'
+        with mock.patch('source.lib.prepare_url', mock.Mock(return_value=url)):
+            with mock.patch('source.lib.get_url', mock.Mock(return_value=[redirect_url, redirect_type, 'content'])):
+                with mock.patch('source.lib.get_counters', mock.Mock(return_value=counters)):
+                    history_types, history_urls, return_counters = lib.get_redirect_history(url, timeout)
+
+        self.assertEquals([redirect_type, redirect_type], history_types)
+        self.assertEquals([url, redirect_url, redirect_url], history_urls)
+        self.assertEquals(counters, return_counters)
 
     #prepare_url(url)
         #positive_tests
--- source/tests/test_lib_worker.py
+++ source/tests/test_lib_worker.py
@@ -4,9 +4,8 @@ from source.lib import worker
 
 
 class LibWorkerTestCase(unittest.TestCase):
-
-    #get_redirect_history_from_task(task, timeout, max_redirects=30, user_agent=None)
-        #positive_tests
+    # get_redirect_history_from_task(task, timeout, max_redirects=30, user_agent=None)
+    #positive_tests
     def test_get_redirect_history_from_task_error_and_not_recheck(self):
         task = mock.Mock()
         task.data = {
@@ -65,9 +64,66 @@ class LibWorkerTestCase(unittest.TestCase):
         with mock.patch('source.lib.worker.get_redirect_history', mock.Mock(return_value=return_value)):
             self.assertEquals((is_input, data_modified), worker.get_redirect_history_from_task(task, timeout))
 
-    #worker(config, parent_pid)
-        #positive_tests
-    """def test_worker(self):
-        config = '/file/path'
+            #worker(config, parent_pid)
+            #positive_tests
+
+    def test_worker_dead_parent(self):
+        config = mock.MagicMock()
         parent_pid = 42
-        with mock.patch('source.lib.worker.get_tube', mock.Mock()):"""
+        tube = mock.MagicMock()
+        with mock.patch('source.lib.worker.get_tube', tube):
+            with mock.patch('os.path.exists', mock.Mock(return_value=False)):
+                self.assertEquals(None, worker.worker(config, parent_pid))
+
+    def test_worker_not_result(self):
+        config = mock.MagicMock()
+        parent_pid = 42
+        tube = mock.MagicMock()
+        with mock.patch('source.lib.worker.get_tube', tube):
+            with mock.patch('os.path.exists', mock.Mock(side_effect=[True, False])) as os_path_exists:
+                with mock.patch('source.lib.worker.get_redirect_history_from_task',
+                                mock.Mock(return_value=None)) as get_redirect_history_from_task:
+                    self.assertEquals(None, worker.worker(config, parent_pid))
+                    assert os_path_exists.call_count == 2
+                    get_redirect_history_from_task.assert_called_once()
+
+    def test_worker_result_is_input(self):
+        config = mock.MagicMock()
+        parent_pid = 42
+        tube = mock.MagicMock()
+        with mock.patch('source.lib.worker.get_tube', tube):
+            with mock.patch('os.path.exists', mock.Mock(side_effect=[True, False])) as os_path_exists:
+                with mock.patch('source.lib.worker.get_redirect_history_from_task',
+                                mock.Mock(return_value=['is_input', 'data'])) as get_redirect_history_from_task:
+                    self.assertEquals(None, worker.worker(config, parent_pid))
+                    assert os_path_exists.call_count == 2
+                    get_redirect_history_from_task.assert_called_once()
+
+    def test_worker_result_not_is_input(self):
+        config = mock.MagicMock()
+        parent_pid = 42
+        tube = mock.MagicMock()
+        with mock.patch('source.lib.worker.get_tube', tube):
+            with mock.patch('os.path.exists', mock.Mock(side_effect=[True, False])) as os_path_exists:
+                with mock.patch('source.lib.worker.get_redirect_history_from_task',
+                                mock.Mock(return_value=[None, 'data'])) as get_redirect_history_from_task:
+                    self.assertEquals(None, worker.worker(config, parent_pid))
+                    assert os_path_exists.call_count == 2
+                    get_redirect_history_from_task.assert_called_once()
+
+         #negative_tests
+    def test_worker_not_result_database_error(self):
+        from source.lib.worker import DatabaseError
+
+        config = mock.MagicMock()
+        parent_pid = 42
+        tube = mock.MagicMock()
+        tube.take = mock.MagicMock()
+        tube.take.ack = mock.Mock(side_effect=DatabaseError)
+        with mock.patch('source.lib.worker.get_tube', tube):
+            with mock.patch('os.path.exists', mock.Mock(side_effect=[True, False])) as os_path_exists:
+                with mock.patch('source.lib.worker.get_redirect_history_from_task',
+                                mock.Mock(return_value=None)) as get_redirect_history_from_task:
+                    self.assertEquals(None, worker.worker(config, parent_pid))
+                    assert os_path_exists.call_count == 2
+                    get_redirect_history_from_task.assert_called_once()
